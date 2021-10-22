2a7e94e41454bb07bf945f69cd3d44bc9b93f28b
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-10-22 15:37:07 +0300

    Added basic game logic.

--- DotNet/NumberGuessingGame/Program.cs
+++ DotNet/NumberGuessingGame/Program.cs
@@ -9,12 +9,29 @@ namespace NumberGuessingGame
             Console.WriteLine("Welcome to the number guessing game!");
             Random random = new();
             int correctAnswer = random.Next(1, 21);
-            Console.WriteLine(correctAnswer);
+            // Console.WriteLine(correctAnswer);
 
             for (int tryNumber = 0; tryNumber < 3; tryNumber++)
             {
-                
+                Console.WriteLine("Enter your guess from 1 to 20:");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess < correctAnswer)
+                {
+                    Console.WriteLine("The correct number is larger.");
+                }
+                else if (guess > correctAnswer)
+                {
+                    Console.WriteLine("The correct number is smaller.");
+                }
+                else
+                {
+                    Console.WriteLine("Correct! You win!");
+                    break;
+                }
             }
+
+            Console.WriteLine("Game has finished, thanks for playing!");
         }
     }
 }
