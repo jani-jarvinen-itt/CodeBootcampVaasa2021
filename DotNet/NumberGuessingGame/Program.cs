e79b129ed3a08bed95a763118d3f12a5b4796b32
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-10-22 15:28:11 +0300

    Added generation of a random number.

--- DotNet/NumberGuessingGame/Program.cs
+++ DotNet/NumberGuessingGame/Program.cs
@@ -6,7 +6,15 @@ namespace NumberGuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the number guessing game!");
+            Random random = new();
+            int correctAnswer = random.Next(1, 21);
+            Console.WriteLine(correctAnswer);
+
+            for (int tryNumber = 0; tryNumber < 3; tryNumber++)
+            {
+                
+            }
         }
     }
 }
