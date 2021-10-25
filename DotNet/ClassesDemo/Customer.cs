7d94b816c5e61da9568951351b6b05d112ffd625
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-10-25 11:21:27 +0300

    Started implementing demo related to C# object oriented programming.

---
+++ DotNet/ClassesDemo/Customer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ClassesDemo
+{
+    class Customer
+    {
+        // properties
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerZipCode { get; set; }
+        public string CustomerCity { get; set; }
+
+    }
+}
---
+++ DotNet/ClassesDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string customerName = "Cecilia Customer";
+            string customerAddress = "Homestreet 1 B 17";
+            string customerZipCode = "40520";
+            string customerCity = "Vaasa";
+
+            PrintCustomer(customerName, customerAddress, customerZipCode, customerCity);
+
+            string customer2Name = "John Doe";
+            string customer2Address = "Pine Street 5 C";
+            string customer2ZipCode = "40550";
+            string customer2City = "Vaasa";
+
+            PrintCustomer(customer2Name, customer2Address, customer2ZipCode, customer2City);
+        }
+
+        private static void PrintCustomer(string name, string address, string zip, string city)
+        {
+            Console.WriteLine("Customer name: " + name);
+            Console.WriteLine("Customer address: " + address);
+            Console.WriteLine("Customer zip: " + zip);
+            Console.WriteLine("Customer city: " + city);
+        }
+    }
+}
