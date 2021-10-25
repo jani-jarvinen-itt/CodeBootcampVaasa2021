d20aba08cedc8c1b829a1afd7fe930213238eee4
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-10-25 11:35:16 +0300

    Implemented first basic version of the Customer class.

--- DotNet/ClassesDemo/Customer.cs
+++ DotNet/ClassesDemo/Customer.cs
@@ -4,11 +4,19 @@ namespace ClassesDemo
 {
     class Customer
     {
-        // properties
-        public string CustomerName { get; set; }
-        public string CustomerAddress { get; set; }
-        public string CustomerZipCode { get; set; }
-        public string CustomerCity { get; set; }
+        // properties (data)
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
 
+        // methods (functionality)
+        public void Print()
+        {
+            Console.WriteLine("Customer name: " + Name);
+            Console.WriteLine("Customer address: " + Address);
+            Console.WriteLine("Customer zip: " + ZipCode);
+            Console.WriteLine("Customer city: " + City);
+        }
     }
 }
--- DotNet/ClassesDemo/Program.cs
+++ DotNet/ClassesDemo/Program.cs
@@ -6,6 +6,22 @@ namespace ClassesDemo
     {
         static void Main(string[] args)
         {
+            Customer first = new();
+            first.Name = "Jane Doe";
+            first.Address = "Wintergarden 123";
+            first.ZipCode = "88200";
+            first.City = "Snowy City";
+            first.Print();
+
+            Customer second = new();
+            second.Name = "Tom Tester";
+            second.Address = "Homestreet 6 B";
+            second.ZipCode = "51000";
+            second.City = "Vaasa";
+            second.Print();
+
+
+            /*
             string customerName = "Cecilia Customer";
             string customerAddress = "Homestreet 1 B 17";
             string customerZipCode = "40520";
@@ -19,14 +35,15 @@ namespace ClassesDemo
             string customer2City = "Vaasa";
 
             PrintCustomer(customer2Name, customer2Address, customer2ZipCode, customer2City);
+            */
         }
 
-        private static void PrintCustomer(string name, string address, string zip, string city)
-        {
-            Console.WriteLine("Customer name: " + name);
-            Console.WriteLine("Customer address: " + address);
-            Console.WriteLine("Customer zip: " + zip);
-            Console.WriteLine("Customer city: " + city);
-        }
+        // private static void PrintCustomer(string name, string address, string zip, string city)
+        // {
+        //     Console.WriteLine("Customer name: " + name);
+        //     Console.WriteLine("Customer address: " + address);
+        //     Console.WriteLine("Customer zip: " + zip);
+        //     Console.WriteLine("Customer city: " + city);
+        // }
     }
 }
