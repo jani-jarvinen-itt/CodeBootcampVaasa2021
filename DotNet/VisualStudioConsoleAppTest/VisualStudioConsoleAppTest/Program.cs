64ad17ba33bd4409aa2699c98450187ebbdb05eb
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-10-26 15:27:26 +0300

    Added two ASP.NET MVC applications and one console application.

---
+++ DotNet/VisualStudioConsoleAppTest/VisualStudioConsoleAppTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml;
+
+namespace VisualStudioConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Visual Studio, change 2!");
+            Console.WriteLine("This is a new line of code.");
+        }
+    }
+}
