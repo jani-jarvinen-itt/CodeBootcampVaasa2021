318322bd3fba454056ccbfea10110a2cfb3b9bc7
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-10-27 11:42:51 +0300

    Added a simple console application to calculate sum of numbers in a text file.

---
+++ DotNet/FileSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "C:\\Bootcamp\\Vaasa\\Numbers.txt";
+            string[] lines = File.ReadAllLines(filename);
+
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                int value = int.Parse(line);
+                sum += value;   // sum = sum + value;
+            }
+
+            Console.WriteLine("Calculated sum is: " + sum);
+        }
+    }
+}
