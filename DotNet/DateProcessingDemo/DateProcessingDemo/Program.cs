85942cbb1cc8e074a0304efce1cfb3a89aaccb29
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-01 10:31:30 +0200

    Added functionality to process invalid dates entered.

--- DotNet/DateProcessingDemo/DateProcessingDemo/Program.cs
+++ DotNet/DateProcessingDemo/DateProcessingDemo/Program.cs
@@ -7,19 +7,31 @@ namespace DateProcessingDemo
         static void Main(string[] args)
         {
             // read the dates from the user
-            Console.WriteLine("Enter the first date:");
-            string value = Console.ReadLine();
-            // int number = int.Parse(value);
-            DateTime date1 = DateTime.Parse(value);
-
-            Console.WriteLine("Enter the second date:");
-            value = Console.ReadLine();
-            DateTime date2 = DateTime.Parse(value);
+            DateTime date1 = ReadDateFromUser("Enter the first date:");
+            DateTime date2 = ReadDateFromUser("Enter the second date:");
 
             // date calculations
             TimeSpan difference = date2 - date1;
             Console.WriteLine("Difference in days: " + difference.TotalDays);
             Console.WriteLine("Difference in hours: " + difference.TotalHours);
-        }        
+        }
+
+        private static DateTime ReadDateFromUser(string prompt)
+        {
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                string value = Console.ReadLine();
+                try
+                {
+                    DateTime date = DateTime.Parse(value);
+                    return date;
+                }
+                catch
+                {
+                    Console.WriteLine("The date entered was invalid. Please try again.");
+                }
+            }
+        }
     }
 }
