929690126c4ab6d53a328170ad98d8bc8a93f081
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-01 10:49:43 +0200

    Added example of the CultureInfo class in datetime processing.

--- DotNet/DateProcessingDemo/DateProcessingDemo/Program.cs
+++ DotNet/DateProcessingDemo/DateProcessingDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace DateProcessingDemo
 {
@@ -11,8 +12,15 @@ namespace DateProcessingDemo
             DateTime date2 = ReadDateFromUser("Enter the second date:");
 
             // display the values entered
-            Console.WriteLine("First date: " + date1);
-            Console.WriteLine("Second date: " + date2);
+            CultureInfo enUS = new("en-US");
+            CultureInfo fr = new("fr");
+            Console.WriteLine("First date (default format): " + date1);
+            Console.WriteLine("Second date (default format): " + date2);
+            Console.WriteLine("First date (en-US): " + date1.ToString(enUS));
+            Console.WriteLine("First date (fr): " + date1.ToString(fr));
+            Console.WriteLine();
+
+            // DateTime parsedDate = DateTime.Parse("11/6/2021", enUS);
 
             // date calculations
             TimeSpan difference = date2 - date1;
