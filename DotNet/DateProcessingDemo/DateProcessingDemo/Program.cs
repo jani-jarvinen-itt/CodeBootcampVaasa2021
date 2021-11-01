201cf47b8f53bffc68139316338084b619e33f72
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-01 10:25:18 +0200

    Added a simple DateTime calculation demo.

---
+++ DotNet/DateProcessingDemo/DateProcessingDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateProcessingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read the dates from the user
+            Console.WriteLine("Enter the first date:");
+            string value = Console.ReadLine();
+            // int number = int.Parse(value);
+            DateTime date1 = DateTime.Parse(value);
+
+            Console.WriteLine("Enter the second date:");
+            value = Console.ReadLine();
+            DateTime date2 = DateTime.Parse(value);
+
+            // date calculations
+            TimeSpan difference = date2 - date1;
+            Console.WriteLine("Difference in days: " + difference.TotalDays);
+            Console.WriteLine("Difference in hours: " + difference.TotalHours);
+        }        
+    }
+}
