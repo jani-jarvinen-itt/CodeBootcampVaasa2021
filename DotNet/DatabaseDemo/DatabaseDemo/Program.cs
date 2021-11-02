6f42aad286c2ef4432fd8a1ba6dbd25013791769
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-02 13:19:35 +0200

    Started implementing the SQL database demo application.

---
+++ DotNet/DatabaseDemo/DatabaseDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
