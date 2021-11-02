87972afb280d6b411a3440eb291ca1b345f1902b
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-02 14:44:07 +0200

    Implemented first working version of SQL database access in C#.

--- DotNet/DatabaseDemo/DatabaseDemo/Program.cs
+++ DotNet/DatabaseDemo/DatabaseDemo/Program.cs
@@ -7,11 +7,24 @@ namespace DatabaseDemo
     {
         static void Main(string[] args)
         {
+            // open connection to the database
             Console.WriteLine("Starting to access the SQL Server database.");
             string connectionString = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
             SqlConnection connection = new(connectionString);
             connection.Open();
             Console.WriteLine("Connection to SQL Server is now open!");
+
+            // execute the SQL query
+            string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+            SqlCommand command = new(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            // process the results
+            while (reader.Read())
+            {
+                string name = reader["CompanyName"].ToString();
+                Console.WriteLine(name);
+            }
         }
     }
 }
