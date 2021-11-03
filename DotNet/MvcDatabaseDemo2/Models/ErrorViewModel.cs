66cc132d3f286599d85c9a59868d474f22042954
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-03 10:37:28 +0200

    Added another example of an ASP.NET MVC application that can access the Northwind database.

---
+++ DotNet/MvcDatabaseDemo2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcDatabaseDemo2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
