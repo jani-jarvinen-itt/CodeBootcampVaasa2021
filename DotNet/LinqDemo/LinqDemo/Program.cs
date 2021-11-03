e8cc3fcd83a990a07d96370abe8bd63ce00a9d70
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-03 14:21:20 +0200

    Added a simple console application to demonstrate LINQ queries in C#.

---
+++ DotNet/LinqDemo/LinqDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // traditional programming
+            int[] numbers = { 3, 6, 10, 4, 7, 11, 5, 9, 12 };
+            Array.Sort(numbers);
+            foreach (int number in numbers)
+            {
+                if (number > 5)
+                {
+                    // Console.WriteLine(number);
+                }
+            }
+
+            // LINQ demo
+            int[] numbers2 = { 3, 6, 10, 4, 7, 11, 5, 9, 12 };
+            var largerThanFive = (from n in numbers2
+                                  where n > 5
+                                  orderby n
+                                  select n);
+            foreach (int number in largerThanFive)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
