e4e3181326b8ea1ccb3653d41175dfe84b0ebaa0
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-04 15:35:49 +0200

    Added fetch call to JSONPlaceholder and an example of state management.

--- React/my-app/src/LifecycleDemo.js
+++ React/my-app/src/LifecycleDemo.js
@@ -5,15 +5,29 @@ class LifecycleDemo extends React.Component {
     constructor(props) {
         super(props);
         console.log("In LifecycleDemo.constructor");
+        this.state = { title: "" };
     }
 
     componentDidMount() {
         console.log("In LifecycleDemo.componentDidMount");
+
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json.title);
+                this.setState({ title: json.title });
+                console.log("Component state has been modified.");
+            });
+        console.log("Fetch call has been made.");
     }
 
     render() {
         console.log("In LifecycleDemo.render");
-        return <h1>Hello, this is the Lifecycle component!</h1>;
+        return (
+            <div>
+                <h1>Hello, this is the Lifecycle component!</h1>
+                <p>Title: {this.state.title}</p>
+            </div>);
     }
 }
 
