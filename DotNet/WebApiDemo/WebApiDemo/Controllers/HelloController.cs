5bd63bf20c6f0b27c531e28dc22dcaf2f64aaa40
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-05 11:18:47 +0200

    Added a new controller for the "Hello" functionality. Also added model class to contain the data.

---
+++ DotNet/WebApiDemo/WebApiDemo/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpPost]
+        public object SayHello(HelloData data)
+        {
+            return new
+            {
+                Text = $"Hello {data.Username} from C#!"
+            };
+        }
+    }
+}
