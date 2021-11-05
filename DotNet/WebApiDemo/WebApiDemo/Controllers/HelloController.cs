c80a39ecb769ee1161d07c099151c3280f37ea54
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-11-05 15:52:03 +0200

    Added an example of adding data to the SQL Server database.

--- DotNet/WebApiDemo/WebApiDemo/Controllers/HelloController.cs
+++ DotNet/WebApiDemo/WebApiDemo/Controllers/HelloController.cs
@@ -16,6 +16,17 @@ namespace WebApiDemo.Controllers
         [Route("")]
         public object SayHello(HelloData data)
         {
+            // example of adding a new row to the SQL database
+            NorthwindContext context = new();
+            Category category = new()
+            {
+                CategoryName = data.Username,
+                Description = "Shoe number: " + data.Shoenumber
+            };
+            context.Categories.Add(category);
+            context.SaveChanges();
+
+            // return JSON data to the caller
             return new
             {
                 Text = $"Hello {data.Username} from C#!"
